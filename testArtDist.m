9829bd0b63805ef8d74fad78471627c9c24fc05c
nicktzr <nicktzr@gmail.com>
2017-08-10 12:28:18 -0700

    Per partner probability fix

--- testArtDist.m
+++
@@ -1,6 +0,0 @@
-   
-tspan = 0 : 0.1 : 100;
-artDist = zeros(prod([disease , viral , gender , age , risk]) , 1);
-artDist = artDist + 100;
-[~ , artTreat] = timeit(ode45(@(t , artDist) treatDist(t , pop1(end , :)) , tspan , artDist)); 
-
