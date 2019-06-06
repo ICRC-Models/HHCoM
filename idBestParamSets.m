7deb2d4e0144f09c6b244ef9366769def6dccbe7
Cara J. Broshkevitch <cjb5ac@virginia.edu>
2019-06-06 08:49:43 -0700

    Account for missing data in idBestParamSets matrix sizing. Upload best sets from May 31.

--- idBestParamSets.m
+++ idBestParamSets.m
@@ -11,19 +11,21 @@ numSubsets = size(negSumLogLmatrix,1)/17;
 negS_format = reshape(negSumLogLmatrix , [17,numSubsets]);
 maxV = max(negS_format(1,:));
 setVec = [1:16:maxV];
+addC = 0;
 for j = 1 : length(setVec)
      if ~any(setVec(j) == negS_format(1,:)) 
-         negS_format(:,end+1) = [setVec(j); ones(16,1).*10000000]; 
+         negS_format(:,end+1) = [setVec(j); ones(16,1).*10000000];
+         addC = addC + 1; 
      end
 end
 
 [temp,firstRowOrder] = sort(negS_format(1,:));
 negS_ordered = negS_format(:,firstRowOrder);
 
-negS_ordered_flatDat = reshape(negS_ordered(2:end,:),[16*numSubsets,1]);
+negS_ordered_flatDat = reshape(negS_ordered(2:end,:),[16*(numSubsets+addC),1]);
 [vals,inds] = sort(negS_ordered_flatDat,'ascend');
 
-file = 'bestParamSets_calib_29May19.dat';
+file = 'bestParamSets_calib_31May19.dat';
 paramDir = [pwd , '/Params/'];
 
 for i = 1:numSets
