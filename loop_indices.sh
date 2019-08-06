b8ebd103c9b068dc482f0ffabe7ffb8f8e1e39e9
Cara J. Broshkevitch <cjb5ac@virginia.edu>
2019-08-06 13:42:13 -0700

    Fixing errors in loop_indices, particularly changing srun to sbatch statements and moving matlab calls into separate scripts. For qos, max jobs is 10. Fixing iinput of string date from bash to matlab.

--- loop_indices.sh
+++ loop_indices.sh
@@ -2,18 +2,15 @@ TCURR=0    # t_curr
 echo "${TCURR}"
 export TCURR
 
-DATE="05Aug19"
+DATE=05Aug19
 echo "${DATE}"
 export DATE
 
 echo "Running MATLAB script to get matrix size."
-srun -p csde -A csde --time=0:10:00 --mem=58G --pty /bin/bash
-export TZ="America/Los_Angeles"
-module load matlab_2018a
-matlab -nodisplay -nosplash -r "idSizeMatrix(${TCURR} , ${DATE})"
-sleep 670
-FILE="matrixSize_calib_$DATE_$TCURR.dat"
-NSETS=$(<$FILE)
+#sbatch -p csde -A csde slurm_sizeMatrix.sbatch
+#sleep 670
+FILE=./Params/matrixSize_calib_${DATE}_${TCURR}.dat
+NSETS=$(<${FILE})
 #NSETS=3088    # nSets
 echo "${NSETS}" 
 export NSETS
@@ -22,8 +19,8 @@ echo "Running simulations, first try."
 INT=0
 for SETIDX in $(seq 1 16 ${NSETS}); do 
 export SETIDX
+sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs10
 INT=$(($INT + 1))
-sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs11
 if [ $INT -ge 10 ]; then 
 sleep 6000 # pause to be kind to the scheduler
 INT=0
@@ -31,12 +28,9 @@ fi
 done
 
 echo "Running MATLAB script to identify failed simulations."
-srun -p csde -A csde --time=00:10:00 --mem=58G --pty /bin/bash
-export TZ="America/Los_Angeles"
-module load matlab_2018a
-matlab -nodisplay -nosplash -r "idMissingSets(${TCURR} , ${DATE} , ${NSETS})"
+sbatch -p csde -A csde slurm_idMissing.sbatch
 sleep 670
-FILE="missingSets_calib_$DATE_$TCURR.dat"
+FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
 RERUN=$(<$FILE)
 echo "$RERUN"
 
@@ -46,8 +40,8 @@ MISSING=($RERUN)
 INT=0
 for SETIDX in ${MISSING[@]}; do
 export SETIDX
+sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs10
 INT=$(($INT + 1))
-sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs11
 if [ $INT -ge 10 ]; then 
 sleep 6000 # pause to be kind to the scheduler
 INT=0
@@ -55,18 +49,13 @@ fi
 done
 
 echo "Running MATLAB script to identify failed simulations, again."
-srun -p csde -A csde --time=00:10:00 --mem=58G --pty /bin/bash
-export TZ="America/Los_Angeles"
-module load matlab_2018a
-matlab -nodisplay -nosplash -r "idMissingSets(${TCURR} , ${DATE} , ${NSETS})"
+sbatch -p csde -A csde slurm_idMissing.sbatch
 sleep 670
-FILE="missingSets_calib_$DATE_$TCURR.dat"
+FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
 RERUN=$(<$FILE)
 echo "$RERUN"
 done
 
 echo "Running MATLAB abc_smc script to get next set of particles."
-srun -p csde -A csde --time=04:00:00 --mem=58G --pty /bin/bash
-export TZ="America/Los_Angeles"
-module load matlab_2018a
-matlab -nodisplay -nosplash -r "abc_smc(${TCURR} , ${DATE} , ${NSETS})"
+sbatch -p csde -A csde slurm_abc.sbatch
+
