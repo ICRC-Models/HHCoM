ff15379fcf557f0b73a4d150d4dae8a4a38ed198
Cara Broshkevitch <carajb@uw.edu>
2019-08-06 11:08:14 -0700

    Enabling automatic rerun of failed simulations.

--- loop_indices.sh
+++ loop_indices.sh
@@ -1,22 +1,72 @@
-NSETS=3088    # nSets
+TCURR=0    # t_curr
+echo "${TCURR}"
+export TCURR
+
+DATE="05Aug19"
+echo "${DATE}"
+export DATE
+
+echo "Running MATLAB script to get matrix size."
+srun -p csde -A csde --time=0:10:00 --mem=58G --pty /bin/bash
+export TZ="America/Los_Angeles"
+module load matlab_2018a
+matlab -nodisplay -nosplash -r "idSizeMatrix(${TCURR} , ${DATE})"
+sleep 670
+FILE="matrixSize_calib_$DATE_$TCURR.dat"
+NSETS=$(<$FILE)
+#NSETS=3088    # nSets
 echo "${NSETS}" 
 export NSETS
 
+echo "Running simulations, first try."
 INT=0
-
-#for SETIDX in $(seq 1 16 ${NSETS}); do 
-MISSING=(529 609 625 689 785 849 945 1009 1169 1265 1297 1329 1361 1425 1489 1585 1649 1745 2273 2369 2385 2929 3009 3025 3089 3105)
-
-for SETIDX in ${MISSING[@]}; do
+for SETIDX in $(seq 1 16 ${NSETS}); do 
 export SETIDX
-
 INT=$(($INT + 1))
-
 sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs11
-
 if [ $INT -ge 10 ]; then 
 sleep 6000 # pause to be kind to the scheduler
 INT=0
 fi 
-
 done
+
+echo "Running MATLAB script to identify failed simulations."
+srun -p csde -A csde --time=00:10:00 --mem=58G --pty /bin/bash
+export TZ="America/Los_Angeles"
+module load matlab_2018a
+matlab -nodisplay -nosplash -r "idMissingSets(${TCURR} , ${DATE} , ${NSETS})"
+sleep 670
+FILE="missingSets_calib_$DATE_$TCURR.dat"
+RERUN=$(<$FILE)
+echo "$RERUN"
+
+echo "Re-running failed simulations."
+while [-z "$RERUN"]; do
+MISSING=($RERUN)
+INT=0
+for SETIDX in ${MISSING[@]}; do
+export SETIDX
+INT=$(($INT + 1))
+sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs11
+if [ $INT -ge 10 ]; then 
+sleep 6000 # pause to be kind to the scheduler
+INT=0
+fi 
+done
+
+echo "Running MATLAB script to identify failed simulations, again."
+srun -p csde -A csde --time=00:10:00 --mem=58G --pty /bin/bash
+export TZ="America/Los_Angeles"
+module load matlab_2018a
+matlab -nodisplay -nosplash -r "idMissingSets(${TCURR} , ${DATE} , ${NSETS})"
+sleep 670
+FILE="missingSets_calib_$DATE_$TCURR.dat"
+RERUN=$(<$FILE)
+echo "$RERUN"
+done
+
+echo "Running MATLAB abc_smc script to get next set of particles."
+srun -p csde -A csde --time=04:00:00 --mem=58G --pty /bin/bash
+export TZ="America/Los_Angeles"
+module load matlab_2018a
+matlab -nodisplay -nosplash -r "abc_smc(${TCURR} , ${DATE} , ${NSETS})"
