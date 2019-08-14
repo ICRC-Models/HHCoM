e29dac1e8d32482e4103a0fba5fbe1c2183e031c
Cara J. Broshkevitch <cjb5ac@virginia.edu>
2019-08-14 11:45:45 -0700

    Uncomment sections of loop_indices for t=1 simulations. Adjust sleep times and run time for slurm_abc.sbatch.

--- loop_indices.sh
+++ loop_indices.sh
@@ -1,4 +1,4 @@
-TCURR=0    # t_curr
+TCURR=1    # t_curr
 echo "${TCURR}"
 export TCURR
 
@@ -6,29 +6,29 @@ DATE=05Aug19
 echo "${DATE}"
 export DATE
 
-#echo "Running MATLAB script to get matrix size."
-#sbatch -p csde -A csde slurm_sizeMatrix.sbatch
-#sleep 670
+echo "Running MATLAB script to get matrix size."
+sbatch -p csde -A csde slurm_sizeMatrix.sbatch
+sleep 600
 FILE=./Params/matrixSize_calib_${DATE}_${TCURR}.dat
 NSETS=$(<${FILE})
 echo "${NSETS}" 
 export NSETS
 
-#echo "Running simulations, first try."
-#INT=0
-#for SETIDX in $(seq 1 16 ${NSETS}); do 
-#export SETIDX
-#sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs10
-#INT=$(($INT + 1))
-#if [ $INT -ge 10 ]; then 
-#sleep 4800 # pause to be kind to the scheduler
-#INT=0
-#fi 
-#done
+echo "Running simulations, first try."
+INT=0
+for SETIDX in $(seq 1 16 ${NSETS}); do 
+export SETIDX
+sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs10
+INT=$(($INT + 1))
+if [ $INT -ge 10 ]; then 
+sleep 4800 # pause to be kind to the scheduler
+INT=0
+fi 
+done
 
-#echo "Running MATLAB script to identify failed simulations."
-#sbatch -p csde -A csde slurm_idMissing.sbatch
-#sleep 300
+echo "Running MATLAB script to identify failed simulations."
+sbatch -p csde -A csde slurm_idMissing.sbatch
+sleep 600
 FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
 RERUN=$(<$FILE)
 echo "$RERUN"
@@ -49,16 +49,16 @@ done
 
 echo "Running MATLAB script to identify failed simulations, again."
 sbatch -p csde -A csde slurm_idMissing.sbatch
-sleep 300
+sleep 600
 FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
 RERUN=$(<$FILE)
 echo "$RERUN"
 done
 
-#echo "Running MATLAB abc_smc script to get next set of particles."
-#sbatch -p csde -A csde slurm_abc.sbatch
-#sleep 7200
+echo "Running MATLAB abc_smc script to get next set of particles."
+sbatch -p csde -A csde slurm_abc.sbatch
+sleep 21600
  
-#echo "Running MATLAB idParamRanges script to get ranges of parameters in best-fitting sets."
-#sbatch -p csde -A csde slurm_idParamRanges.sbatch
+echo "Running MATLAB idParamRanges script to get ranges of parameters in best-fitting sets."
+sbatch -p csde -A csde slurm_idParamRanges.sbatch
 
