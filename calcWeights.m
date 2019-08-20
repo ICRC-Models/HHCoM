87cee095d723fa6f62be8bb7045ea106e0b16bb8
Cara J. Broshkevitch <cjb5ac@virginia.edu>
2019-08-20 14:01:42 -0700

    Parallelize weight calculation in abc_smc.

--- calcWeights.m
+++ calcWeights.m
@@ -24,15 +24,17 @@ const = (1/sqrt(2 * pi));
 denominator = zeros(numPart,1);
   
 % For each particle simulated in iteration t, calculate the probability of moving to that particle from each accepted particle in iteration t-1
-for i = 1 : numPart
+parfor i = 1 : numPart
+    denominator_temp = 0.0;
     for j = 1 : numPart_prev
         kernel = 1;
         for p = 1 : numParam
             v = 2 .* var(alphaSets_prev(p,:)); % 2x variance of previous accepted particles (normalized by numPart-1)
             kernel = kernel * (const * (1 / sqrt(v)) * exp(-0.5 * ((1/v)*(paramSetMatrix(p,i) - alphaSets_prev(p,j))) ^ 2));
         end
-        denominator(i) = denominator(i) + alphaWeights_prev(j) * kernel;
+        denominator_temp = denominator_temp + alphaWeights_prev(j) * kernel;
     end
+    denominator(i) = denominator_temp;
 end
   
 %% Calculate weights
