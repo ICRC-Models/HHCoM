7f75551d5a094add3f3e8aecf9c20322ad9423bc
Cara Broshkevitch <carajb@uw.edu>
2019-08-29 15:59:20 -0700

    Add labels to vaxCEA_whoP2_082219.m better describing what needs to be changed.

--- vaxCEA_whoP2_082219.m
+++ vaxCEA_whoP2_082219.m
@@ -18,6 +18,7 @@ currYear = 2020; %c(1); % get the current year from time
 
 %% LOAD SAVED RESULTS
 curr = load([pwd , '\HHCoM_Results\toNow_082119_singleAge_baseScreen_noBaseVax_2020']); % ***SET ME***: name for historical run file
+% ***SET ME***: save names of potential scenarios to analyze as variables
 dirName_reductBaseline = '082119_WHOP1_SCE1';
 dirName_P1_SCE3 = '082119_WHOP1_SCE3';
 dirName_P1_SCE5 = '082119_WHOP1_SCE5';
@@ -28,8 +29,11 @@ dirName_P2_SCE4 = '082119_WHOP2_SCE4';
 dirName_P2_SCE5 = '082119_WHOP2_SCE5';
 dirName_P2_SCE1_2xHIVneg = '082119_WHOP2_SCE1_2xHIVneg';
 dirName_P2_SCE2_2xHIVneg = '082119_WHOP2_SCE2_2xHIVneg';
+
+% ***SET ME***: choose which scenarios you want to save data in Excel for
 simVec = {dirName_reductBaseline , dirName_P1_SCE3 , dirName_P1_SCE5 , dirName_P2_SCE1 , dirName_P2_SCE2 , dirName_P2_SCE3 , dirName_P2_SCE4 , dirName_P2_SCE5}; %{dirName_P2_SCE1_2xHIVneg , dirName_P2_SCE2_2xHIVneg};
-fileTits = {'P1_SCE1' , 'P1_SCE3' , 'P1_SCE5' , 'P2_SCE1' , 'P2_SCE2' , 'P2_SCE3' , 'P2_SCE4' ,'P2_SCE5'}; %{'P2_SCE1x2neg' , 'P2_SCE2x2neg'};
+% ***SET ME***: make sure the names here correspond to scenarios in simVec above
+fileTits = {'P1_SCE1' , 'P1_SCE3' , 'P1_SCE5' , 'P2_SCE1' , 'P2_SCE2' , 'P2_SCE3' , 'P2_SCE4' , 'P2_SCE5'}; %{'P2_SCE1x2neg' , 'P2_SCE2x2neg'};
 
 nResults = length(simVec);
 
@@ -336,5 +340,60 @@ for j = 1 : nResults
             
     end
     
+    %% HPV PREVALENCE REDUCTION - age standardized
+%     inds = {':'}; % HIV state inds
+%     plotTits1 = {'General'};
+%     fac = 10 ^ 5;
+%     
+%     %worldStandard_WP2015 = [325428 311262 295693 287187 291738 299655 272348 ...
+%     %    247167 240167 226750 201603 171975 150562 113118 82266 64484];
+%     worldStandard_WP2015 = [325428 (311262/5.0) 295693 287187 291738 299655 272348 ...
+%         247167 240167 226750 201603 171975 150562 113118 82266 64484];
+%     
+%     %aVec = {1:5,6:10,11:15,16:20,21:25,26:30,31:35,36:40,41:45,46:50,51:55,56:60,61:65,66:70,71:75,76:80};
+%     aVec = {1:5,10,11:15,16:20,21:25,26:30,31:35,36:40,41:45,46:50,51:55,56:60,61:65,66:70,71:75,76:80};
+%     
+% 
+%     for n = 1 : nSims
+%         vaxResult{n}.hpvPrevRef = zeros(length(tVec(1 : stepsPerYear : end)),1)';
+%     end
+% 
+%     for aInd = 2:(age/5)-1 %1 : (age/5)
+%         a = aVec{aInd};
+% 
+%         % General
+%         hpv = toInd(allcomb(1 : disease , 1 : viral , 2 : hpvTypes , 1 : 7 , ...
+%             1 : periods , 1 : gender , a , 1 : risk));
+%         popTot = toInd(allcomb(1 : disease , 1 : viral , 1 : hpvTypes , 1 : hpvStates , ...
+%             1 : periods , 1 : gender , a , 1 : risk));
+% 
+%         % Calculate hpv prevalence
+%         for n = 1
+%             hpvPrevRef = ...
+%                 (annlz(sum(vaxResult{n}.popVec(: , hpv) , 2)) ./ ...
+%                 annlz(sum(vaxResult{n}.popVec(: , popTot) , 2))) ...
+%                 .* (worldStandard_WP2015(aInd));
+%             vaxResult{n}.hpvPrevRef = vaxResult{n}.hpvPrevRef + hpvPrevRef;
+%         end
+%     end
+%     for n = 1 : nSims
+%         vaxResult{n}.hpvPrev = vaxResult{n}.hpvPrevRef ./ (sum(worldStandard_WP2015(2:(age/5)-1))); %(1:(age/5))));
+%     end
+% 
+%     % Save hpv prevalence results
+%     for n = 1 : nSims
+%         fname = [pwd , '\HHCoM_Results\Vaccine' , dirName_reductBaseline , '\' , fileTits{j} , ...
+%             'Coverage' , num2str(round(vaxResult{n}.vaxRate * 100)) , '_AgeStandHpvPrev_ages9-74' , '.xlsx'];
+%         sname = plotTits1{1};
+%         if exist(fname , 'file') == 2
+%             M = xlsread(fname);
+%             M = catpad(2 , [tVec(1 : stepsPerYear : end)' , vaxResult{n}.hpvPrev'] , M);
+%             xlswrite(fname , M , sname)
+%         else
+%             xlswrite(fname , [tVec(1 : stepsPerYear : end)' , vaxResult{n}.hpvPrev'] , sname)
+%         end
+%     end
+
     clear vaxResult; 
 end
+
