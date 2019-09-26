4cdf811396ea7f2559f2a4b6b07b4c8a20a5c38a
Cara J. Broshkevitch <cjb5ac@virginia.edu>
2019-09-26 15:35:08 -0700

    Update routine vaccination to handle multiple age groups.

--- hpvVaxSchool.m
+++ hpvVaxSchool.m
@@ -16,28 +16,31 @@ for d = 1 : disease
     for v = 1 : viral
         for g = min(vaxG) : max(vaxG) 
             for r = 1 : risk
-                fromNonVSus = toInd(allcomb(d , v , 1 , 1 , 1 , ... 
-                    g , vaxAge , r));
-                fromNonVImm = toInd(allcomb(d , v , 2 , 10 , 1 , ... 
-                    g , vaxAge , r));
-                toV = toInd(allcomb(d , v , 1 , 9 , 1 , ...
-                    g , vaxAge , r));
-                otherV = toInd(allcomb(d , v , 1 : hpvTypes , 1 : hpvStates , ...
-                    2 , g , vaxAge , r));
-                allVNonV = toInd(allcomb(d , v , 1 : hpvTypes , 1 :hpvStates , ...
-                    1 : periods , g , vaxAge , r)); 
+                for aV = 1 : length(vaxAge)
+                    a = vaxAge(aV);
+	            fromNonVSus(:,aV) = toInd(allcomb(d , v , 1 , 1 , 1 , ... 
+                        g , a , r));
+                    fromNonVImm(:,aV) = toInd(allcomb(d , v , 2 , 10 , 1 , ... 
+                        g , a , r));
+                    toV(:,aV) = toInd(allcomb(d , v , 1 , 9 , 1 , ...
+                        g , a , r));
+                    otherV(:,aV) = toInd(allcomb(d , v , 1 : hpvTypes , 1 : hpvStates , ...
+                        2 , g , a , r));
+                    allVNonV(:,aV) = toInd(allcomb(d , v , 1 : hpvTypes , 1 :hpvStates , ...
+                        1 : periods , g , a , r)); 
 
-                fracVaxd = (sumall(pop(toV)) + sumall(pop(otherV))) / ... % find proportion of population that is currently vaccinated
-                    (sumall(pop(allVNonV)));
-                if vaxRate - fracVaxd > 10 ^ -6 % when proportion vaccinated is below target vaccination level
-                    vaxCover = max(0 , (vaxRate - fracVaxd) ./ (1 - fracVaxd)); % vaccinate enough people in age group to reach target
-                    vaxdGroupSus = vaxCover .* pop(fromNonVSus);
-                    vaxdGroupImm = vaxCover .* pop(fromNonVImm);
-                    dPop(fromNonVSus) = dPop(fromNonVSus) - vaxdGroupSus;
-                    dPop(fromNonVImm) = dPop(fromNonVImm) - vaxdGroupImm;
-                    dPop(toV) = dPop(toV) + vaxdGroupSus + vaxdGroupImm;
-                    hpvVaxd = hpvVaxd + sumall(vaxdGroupSus) + sumall(vaxdGroupImm); % count number of people vaccinated at current time step
-                end
+                    fracVaxd = (sumall(pop(toV(:,aV))) + sumall(pop(otherV(:,aV)))) / ... % find proportion of population that is currently vaccinated
+                        (sumall(pop(allVNonV(:,aV))));
+                    if vaxRate - fracVaxd > 10 ^ -6 % when proportion vaccinated is below target vaccination level
+                        vaxCover = max(0 , (vaxRate - fracVaxd) ./ (1 - fracVaxd)); % vaccinate enough people in age group to reach target
+                        vaxdGroupSus = vaxCover .* pop(fromNonVSus(:,aV));
+                        vaxdGroupImm = vaxCover .* pop(fromNonVImm(:,aV));
+                        dPop(fromNonVSus(:,aV)) = dPop(fromNonVSus(:,aV)) - vaxdGroupSus;
+                        dPop(fromNonVImm(:,aV)) = dPop(fromNonVImm(:,aV)) - vaxdGroupImm;
+                        dPop(toV(:,aV)) = dPop(toV(:,aV)) + vaxdGroupSus + vaxdGroupImm;
+                        hpvVaxd = hpvVaxd + sumall(vaxdGroupSus) + sumall(vaxdGroupImm); % count number of people vaccinated at current time step
+                    end
+		end
             end
         end
     end
