bf4814024b8341148ee0b3a9cb71ccca6bf67f8f
Cara Bayer <carajb@mox1.hyak.local>
2019-11-01 15:27:37 -0700

    Update loop_indices to run on checkpoint queue.

--- loop_indices.sh
+++ loop_indices.sh
@@ -6,9 +6,9 @@ DATE=31Oct19
 echo "${DATE}"
 export DATE
 
-echo "Running MATLAB script to get matrix size."
-sbatch -p csde -A csde slurm_sizeMatrix.sbatch
-sleep 300
+#echo "Running MATLAB script to get matrix size."
+#sbatch -p ckpt -A csde-ckpt slurm_sizeMatrix.sbatch
+#sleep 300
 FILE=./Params/matrixSize_calib_${DATE}_${TCURR}.dat
 NSETS=$(<${FILE})
 echo "${NSETS}" 
@@ -18,47 +18,47 @@ echo "Running simulations, first try."
 INT=0
 for SETIDX in $(seq 1 16 ${NSETS}); do 
 export SETIDX
-sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs10
+sbatch -p ckpt -A csde-ckpt slurm_batch.sbatch --qos=MaxJobs10
 INT=$(($INT + 1))
 if [ $INT -ge 10 ]; then 
-sleep 4800 # pause to be kind to the scheduler
+sleep 60 #4800 # pause to be kind to the scheduler
 INT=0
 fi 
 done
 
-echo "Running MATLAB script to identify failed simulations."
-sbatch -p csde -A csde slurm_idMissing.sbatch
-sleep 300
-FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
-RERUN=$(<$FILE)
-echo "$RERUN"
+#echo "Running MATLAB script to identify failed simulations."
+#sbatch -p ckpt -A csde-ckpt slurm_idMissing.sbatch
+#sleep 300
+#FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
+#RERUN=$(<$FILE)
+#echo "$RERUN"
 
-echo "Re-running failed simulations."
-while [ ! -z "$RERUN" ]; do
-MISSING=($RERUN)
-INT=0
-for SETIDX in ${MISSING[@]}; do
-export SETIDX
-sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs10
-INT=$(($INT + 1))
-if [ $INT -ge 10 ] || [ $INT -eq ${#MISSING[@]} ]; then 
-sleep 4800 # pause to be kind to the scheduler
-INT=0
-fi 
-done
+#echo "Re-running failed simulations."
+#while [ ! -z "$RERUN" ]; do
+#MISSING=($RERUN)
+#INT=0
+#for SETIDX in ${MISSING[@]}; do
+#export SETIDX
+#sbatch -p ckpt -A csde-ckpt slurm_batch.sbatch --qos=MaxJobs10
+#INT=$(($INT + 1))
+#if [ $INT -ge 10 ] || [ $INT -eq ${#MISSING[@]} ]; then 
+#sleep 4800 # pause to be kind to the scheduler
+#INT=0
+#fi 
+#done
 
-echo "Running MATLAB script to identify failed simulations, again."
-sbatch -p csde -A csde slurm_idMissing.sbatch
-sleep 300
-FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
-RERUN=$(<$FILE)
-echo "$RERUN"
-done
+#echo "Running MATLAB script to identify failed simulations, again."
+#sbatch -p ckpt -A csde-ckpt slurm_idMissing.sbatch
+#sleep 300
+#FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
+#RERUN=$(<$FILE)
+#echo "$RERUN"
+#done
 
-echo "Running MATLAB abc_smc script to get next set of particles."
-sbatch -p csde -A csde slurm_abc.sbatch
-sleep 21600
+#echo "Running MATLAB abc_smc script to get next set of particles."
+#sbatch -p csde -A csde slurm_abc.sbatch
+#sleep 21600
  
-echo "Running MATLAB idParamRanges script to get ranges of parameters in best-fitting sets."
-sbatch -p csde -A csde slurm_idParamRanges.sbatch
+#echo "Running MATLAB idParamRanges script to get ranges of parameters in best-fitting sets."
+#sbatch -p ckpt -A ckpt-csde slurm_idParamRanges.sbatch
 
