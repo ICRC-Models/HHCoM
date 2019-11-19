4d9232a1761dc7489ecb029205252ace478aa95c
guiliu53 <guiliu@uw.edu>
2019-11-19 13:11:43 -0800

    updated for running on Hyak

--- lhsPatternSrch.m
+++ lhsPatternSrch.m
@@ -7,12 +7,12 @@
 % 2) File: paramSets_calib_[date].dat (sample of parameter sets [number
 % parameters x number samples])
 
-function lhsPatternSrch(tstep_abc , date_abc)
+function lhsPatternSrch (t_curr , date)
 
 %delete(gcp('nocreate')); 
 
-t_curr = tstep_abc;
-date = date_abc;
+% t_curr = 0;
+% date = '29Aug19';
 
 %% Load parameters
 paramDir = [pwd ,'/Params/'];
@@ -21,15 +21,15 @@ load([paramDir,'general'])
 
 %% Cluster information
 pc = parcluster('local');    % create a local cluster object
-pc.JobStorageLocation = strcat('/gscratch/csde/carajb' , '/' , getenv('SLURM_JOB_ID'))    % explicitly set the JobStorageLocation to the temp directory that was created in the sbatch script
+pc.JobStorageLocation = strcat('/gscratch/csde/guiliu' , '/' , getenv('SLURM_JOB_ID'))    % explicitly set the JobStorageLocation to the temp directory that was created in the sbatch script
 parpool(pc , str2num(getenv('SLURM_CPUS_ON_NODE')))    % start the pool with max number workers
 
 %% Load structure of all potentially calibrated parameters
 [paramsAll] = genParamStruct();
 
 %% Latin hypercube sampling of parameter space
-nSets = 50;
-pIdx = [1,2,5,6,7,8,9,10,19,22,25];    % indices in paramsAll cell array
+nSets = 1;
+pIdx = [1,2,3,4,5,6,7,8,9,10];    % indices in paramsAll cell array
 
 paramsSub = cell(length(pIdx),1);
 p = 0;
@@ -45,8 +45,8 @@ for s = 1 : length(pIdx)
     lb = [lb; paramsSub{s}.lb];
     ub = [ub; paramsSub{s}.ub];
 end
-lb(8) = lb(8).*10; % re-scale perPartnerHpv to be more similar in scale to other params
-ub(8) = ub(8).*10;
+lb(86) = lb(86).*10; % re-scale perPartnerHpv to be more similar in scale to other params
+ub(86) = ub(86).*10;
 
 sampleNorm = lhsdesign(nSets , p , 'smooth' , 'off');    % latin hypercube sampling
 
