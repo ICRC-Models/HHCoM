f018e662fecf7f0af776ff5c92bbb3b5f4eec77e
Cara Bayer <carajb@uw.edu>
2019-12-10 14:52:25 -0800

    Modify loop_indices to submit jobs to nodes of different numbers of cores in sections.

--- loop_indices.sh
+++ loop_indices.sh
@@ -2,30 +2,45 @@ TCURR=0    # t_curr
 echo "${TCURR}"
 export TCURR
 
-DATE=5Dec19
+DATE=10Dec19
 echo "${DATE}"
 export DATE
 
-#echo "Running MATLAB script to get matrix size."
-#sbatch -p csde -A csde slurm_sizeMatrix.sbatch
-#sleep 300
+echo "Running MATLAB script to get matrix size."
+batch -p csde -A csde slurm_sizeMatrix.sbatch
+sleep 300
 FILE=./Params/matrixSize_calib_${DATE}_${TCURR}.dat
 NSETS=$(<${FILE})
 echo "${NSETS}" 
 export NSETS
 
 echo "Running simulations, first try."
-INT=0
-for SETIDX in $(seq 1 40 3200); do
-#for SETIDX in $(seq 3201 32 5760); do
-#for SETIDX in $(seq 5761 28 ${NSETS}); do
-export SETIDX
-sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4
-INT=$(($INT + 1))
-if [ $INT -ge 50 ]; then 
-sleep 5 #4800 # pause to be kind to the scheduler
-INT=0
-fi 
+SEQ40=($(seq 1 40 3200 ))
+SEQ32=($(seq 3201 32 5760))
+SEQ28p=($(seq 5761 28 8000))
+SEQ28s=($(seq 8001 28 ${NSETS}))    # set up for NSETS=10240
+LENGTH40=${#SEQ40[@]}    # note: all three lengths should be the same
+LENGTH32=${#SEQ32[@]}
+LENGTH28=${#SEQ28p[@]}
+echo "${LENGTH40}"
+echo "${LENGTH32}"
+echo "${LENGTH28}"
+for i in $(seq 1 4 ${LENGTH40}); do
+    for j in $(seq $((${i}-1)) 1 $((${i}+3))); do    # submit 4 simulations for each target node at once
+        SETIDX=${SEQ40[$j]}
+		export SETIDX
+		sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=40
+        SETIDX32=${SEQ32[$j]}
+		export SETIDX
+		sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=32
+        SETIDX28p=${SEQ28p[$j]}
+		export SETIDX
+		sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=28
+        SETIDX28s=${SEQ28s[$j]}
+		export SETIDX
+		sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=28
+    done
+	sleep 19200    # give submitted simulations time to finish 
 done
 
 #echo "Running MATLAB script to identify failed simulations."
