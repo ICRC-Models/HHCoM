ba3bbecae84158d7357eb62bcdf1eaa744e3c22b
Cara Bayer <carajb@uw.edu>
2019-12-11 12:24:20 -0800

    First draft of updating logic for identifying and resubmitting missing sims to nodes of variable core number.

--- loop_indices.sh
+++ loop_indices.sh
@@ -44,7 +44,7 @@ for i in $(seq 1 4 ${LENGTH40}); do
 done
 
 #echo "Running MATLAB script to identify failed simulations."
-#sbatch -p ckpt -A csde-ckpt slurm_idMissing.sbatch
+#sbatch -p csde -A csde slurm_idMissing.sbatch
 #sleep 300
 #FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
 #RERUN=$(<$FILE)
@@ -52,24 +52,26 @@ done
 
 #echo "Re-running failed simulations."
 #while [ ! -z "$RERUN" ]; do
-#MISSING=($RERUN)
-#INT=0
-#for SETIDX in ${MISSING[@]}; do
-#export SETIDX
-#sbatch -p ckpt -A csde-ckpt slurm_batch.sbatch --qos=MaxJobs4
-#INT=$(($INT + 1))
-#if [ $INT -ge 50 ] || [ $INT -eq ${#MISSING[@]} ]; then 
-#sleep 5 #4800 # pause to be kind to the scheduler
-#INT=0
-#fi 
-#done
+    #MISSING=($RERUN)
+    ********
+	#INT=0
+    #for SETIDX in ${MISSING[@]}; do
+        #export SETIDX
+        #sbatch -p ckpt -A csde-ckpt slurm_batch.sbatch --qos=MaxJobs4
+        #INT=$(($INT + 1))
+        #if [ $INT -ge 50 ] || [ $INT -eq ${#MISSING[@]} ]; then 
+            #sleep 5 #4800 # pause to be kind to the scheduler
+            #INT=0
+        #fi 
+    #done
+    *********
 
-#echo "Running MATLAB script to identify failed simulations, again."
-#sbatch -p ckpt -A csde-ckpt slurm_idMissing.sbatch
-#sleep 300
-#FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
-#RERUN=$(<$FILE)
-#echo "$RERUN"
+    #echo "Running MATLAB script to identify failed simulations, again."
+    #sbatch -p csde -A csde slurm_idMissing.sbatch
+    #sleep 300
+    #FILE=./Params/missingSets_calib_${DATE}_${TCURR}.dat
+    #RERUN=$(<$FILE)
+    #echo "$RERUN"
 #done
 
 #echo "Running MATLAB abc_smc script to get next set of particles."
