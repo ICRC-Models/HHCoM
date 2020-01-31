4f922493889fbdd0a2fa1a2e371e5f4883f9282e
Cara Bayer <carajb@uw.edu>
2020-01-31 13:53:47 -0800

    Add HPV protection factor from circumcision
    Turn on protection against male HPV acquistion with male neonatal
    circumcision.
    
    Closes #26

--- loadUp2.m
+++ loadUp2.m
@@ -669,6 +669,10 @@ end
 load([paramDir , 'hivIntParamsFrmExcel'] , 'circ' , 'circProtect' , ...
     'condProtect' , 'MTCTRate');
 
+% Protection from circumcision and condoms
+circProtect = [[circProtect; 0.0] , [0.30; 0.0]];    % HIV protection , HPV protection
+condProtect = [ones(gender,1).*condProtect , [0.0; 0.0]]; % [0.46; 0.70]    % HIV protection , HPV protection
+
 % Condom use
 if calibBool && any(5 == pIdx);
     idx = find(5 == pIdx);
