84136dad3cd1574fb7d05194885f706b03edd028
Cara Bayer <carajb@uw.edu>
2020-02-07 11:35:56 -0800

    Fix discontinuation to maintain pop-level ART coverage
    If population-level ART coverage is too high, we discontinue persons
    proportionately by age to achieve the proper coverage, distributing
    proportionately the discontinuations that would have occured in age
    groups already at minimum coverage.
    
    I had incorrectly calculated current proportions on ART for
    discontinuation as the proportion HIV-positive which makes no sense.
    The adjustment factor that accounts for previous population changes to
    maintain coverage limits is therefore much simpler.
    
    Fixes #61

--- artPopCov.m
+++ artPopCov.m
@@ -15,24 +15,20 @@ fracARTAge_adj(1 , maxAges) = maxCoverLim; % adjust for ages brought down to max
 fracART = sum(fracARTAge_adj(1 , ageSexDebut:end) .* ...
     (agePopSubTots(1 , ageSexDebut:end) ./ sum(agePopSubTots(1 , ageSexDebut:end)))); % calculate weighted average of adjusted age-level coverages
 
-fracExcMinAges = ageHIVSubTots(excMinAges) ./ sum(ageHIVSubTots(excMinAges)); % population HIV proportion by age, for ages > MIN coverage
-toRedisMinAges = sum(ageHIVSubTots(minAges)); % summed HIV-positives of ages < MIN to redistribute
+fracExcMinAges = ageARTSubTots(excMinAges) ./ sum(ageARTSubTots(excMinAges)); % population ART proportion by age, for ages > MIN coverage
+toRedisMinAges = sum(ageARTSubTots(minAges)); % summed HIV-positives on ART of ages < MIN to redistribute
 % Discontinue persons to match population coverage
 if fracART > popCoverInd
     cover = (fracART - popCoverInd) ./ (fracART); % discontinuation fraction needed to meet population-level coverage
     totToRedis = fracExcMinAges .* sumall(toRedisMinAges .* max(cover , 0)); % calculate discontinuation #s, distributing persons that would have been from ages < MIN
-    fracToRedis = totToRedis ./ ageHIVSubTots(excMinAges); % calculate discontinuation proportions
+    fracToRedis = totToRedis ./ ageARTSubTots(excMinAges); % calculate discontinuation proportions
     formatToRedis = ones(1 , length(excMinAges) , 1);
     formatToRedis(:) = (max(cover , 0) + fracToRedis'); % set up matrix values by age
-    formatModifier = ones(1 , length(excMinAges) , 1);
-    formatModifier(:) = ageARTSubTots ./ ageHIVSubTots;
     % Discontinuation matrix by gender, age, risk, accounting for min/max adjustment
-    % adjustment factor = ((HIVpop+#discont)/HIVpop) = (1 + #discont/HIVpop) 
-    % = (1 + ((artOut*ARTpop)/HIVpop)) = (1 + artOut * (ARTpop/HIVpop))
+    % adjustment factor = ((ARTpop-#disc)/ARTpop) = (1 - #disc/ARTpop) = (1 - artOut)
     artOut(g , excMinAges , :) = artOut(g , excMinAges , :) + ...
         bsxfun(@times , ones(1 , length(excMinAges) , risk) , formatToRedis) .* ...
-        (1 + artOut(g , excMinAges , :) .* ...
-        bsxfun(@times , ones(1 , length(excMinAges) , risk) , formatModifier));
+        (1 - artOut(g , excMinAges , :));
 end
 
 fracExcMaxAges = ageHIVSubTots(excMaxAges) ./ sum(ageHIVSubTots(excMaxAges)); % population HIV proportion by age, for ages < MAX coverage
@@ -45,7 +41,7 @@ if fracART < popCoverInd
     formatToRedis = ones(1 , 1 , 1 , length(excMaxAges) , 1);
     formatToRedis(:) = (max(cover , 0) + fracToRedis'); % set up matrix values by age
     % Initiation matrix by disease, VL, gender, age, risk, accounting for min/max adjustment
-    % adjustment factor = ((HIVpop-#init)/HIVpop) = = (1 - #init/HIVpop) = (1 - treat)
+    % adjustment factor = ((HIVpop-#init)/HIVpop) = (1 - #init/HIVpop) = (1 - treat)
     treat(dRange , 1 : 5 , g , excMaxAges , :) = treat(dRange , 1 : 5 , g , excMaxAges , :) + ...
         bsxfun(@times , ones(length(dRange) , 5 , 1 , length(excMaxAges) , risk) , formatToRedis) .* ...
         (1 - treat(dRange , 1 : 5 , g , excMaxAges , :)); 
