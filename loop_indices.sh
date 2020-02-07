6c739b94846d9a1cb004e71a7297fac59ddaba6f
Cara Bayer <carajb@mox1.hyak.local>
2020-02-07 15:27:53 -0800

    Streamline loop_indices slurm script
    I had tried to write code that would run more simulations in
    parallel on nodes with more cores. However, I could never get
    this to work. Runs would run out of memory and fail on 32 and 40
    cpu nodes. I think this is because they had different/less
    working memory. Removing this code to streamline script.

--- loop_indices.sh
+++ loop_indices.sh
@@ -15,32 +15,11 @@ echo "${NSETS}"
 export NSETS
 
 echo "Running simulations, first try."
-#SEQ40=($(seq 1 40 3200 ))
-#SEQ32=($(seq 3201 32 5760))
-#SEQ28p=($(seq 5761 28 8000))
-#SEQ28s=($(seq 8001 28 ${NSETS}))    # set up for NSETS=10240
 SEQ28all=($(seq 1 28 ${NSETS}))      # set up for NSETS=10220
-#LENGTH40=${#SEQ40[@]}    # note: all three lengths should be the same
-#LENGTH32=${#SEQ32[@]}
-#LENGTH28=${#SEQ28p[@]}
 LENGTH28=${#SEQ28all[@]}
-#echo "${LENGTH40}"
-#echo "${LENGTH32}"
 echo "${LENGTH28}"
 for i in $(seq 1 5 ${LENGTH28}); do
-    for j in $(seq $((${i}-1)) 1 $((${i}+3))); do    # submit 4 simulations for each target node at once
-#       #SETIDX=${SEQ40[$j]}
-#	#	export SETIDX
-#	#	sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=40 --mem=185G 
-#       #SETIDX=${SEQ32[$j]}
-#	#	export SETIDX
-#	#	sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=32 --mem=248G 
-#       #SETIDX=${SEQ28p[$j]}
-#	#	export SETIDX
-#	#	sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=28 --mem=248G 
-#       #SETIDX=${SEQ28s[$j]}
-#	#	export SETIDX
-#	#	sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=28 --mem=248G 
+    for j in $(seq $((${i}-1)) 1 $((${i}+3))); do    # submit 4 simulations for each target node at once 
         SETIDX=${SEQ28all[$j]}
                 export SETIDX
                 sbatch -p csde -A csde slurm_batch.sbatch #--qos=MaxJobs4
@@ -62,30 +41,6 @@ while [ ! -z "$RERUN" ]; do
     INT=0
     for i in $(seq 1 5 ${LENGTH28}); do
 	for j in $(seq $((${i}-1)) 1 $((${i}+3))); do    # submit 4 simulations for each target node at once
-        #    SETIDX=${SEQ40[$j]}
-        #    if [[ " ${MISSING[@]} " =~ " ${SETIDX} " ]]; then
-	#	export SETIDX
-	#	sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=40 --mem=185G
-	#        INT=$(($INT + 1))
-	#    fi
-        #    SETIDX=${SEQ32[$j]}
-	#    if [[ " ${MISSING[@]} " =~ " ${SETIDX} " ]]; then
-	#	export SETIDX
-	#        sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=32 --mem=248G
-	#	INT=$(($INT + 1))
-        #    fi
-        #    SETIDX=${SEQ28p[$j]}
-	#    if [[ " ${MISSING[@]} " =~ " ${SETIDX} " ]]; then
-	#    	export SETIDX
-        #		sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=28 --mem=248G
-	#        INT=$(($INT + 1))
-	#    fi
-        #    SETIDX=${SEQ28s[$j]}
-	#    if [[ " ${MISSING[@]} " =~ " ${SETIDX} " ]]; then
-	#    	export SETIDX
-        #		sbatch -p csde -A csde slurm_batch.sbatch --qos=MaxJobs4 --ntasks-per-node=28 --mem=248G
-        #        INT=$(($INT + 1))
-	#    fi
              SETIDX=${SEQ28all[$j]}
              if [[ " ${MISSING[@]} " =~ " ${SETIDX} " ]]; then
                  export SETIDX
@@ -109,12 +64,13 @@ while [ ! -z "$RERUN" ]; do
     RERUN=$(<$FILE)
     echo "$RERUN"
 done
-END
 
-#echo "Running MATLAB abc_smc script to get next set of particles."
-#sbatch -p csde -A csde slurm_abc.sbatch
-#sleep 21600
+echo "Running MATLAB abc_smc script to get next set of particles."
+sbatch -p csde -A csde slurm_abc.sbatch
+sleep 21600
  
 #echo "Running MATLAB idParamRanges script to get ranges of parameters in best-fitting sets."
 #sbatch -p csde -A csde slurm_idParamRanges.sbatch
+
+END
 
