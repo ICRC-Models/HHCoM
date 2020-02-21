a3dcc34394a2ccb4246f65fc85cc86ea823370a8
Cara Bayer <carajb@uw.edu>
2020-02-21 14:49:58 -0800

    Improve robustness of ART coverage by age algorithm
    Address bug encountered when I implemented the current ART coverage by age
    adjustment algorithm in the master branch. Didn't see this bug in
    current model version, but modifying code to improve robustness.
    -Use ageSexDebut variable instead of hardcoding age
    -Ages less than ageSexDebut may have NaN values. Update logic and
    accordingly and ensure exclusion of ages ineligible for ART in excMinAges.
    
    Closes #61

--- artMinMax.m
+++ artMinMax.m
@@ -11,7 +11,7 @@ maxInds = ageFracART > maxCoverLim; % find inds of ages above max coverage
 if any(maxInds)
     maxAges = ageVec(maxInds == 1); % ages above max coverage
     excMaxAges = ageVec(~maxInds); % ages that are good, ie below max coverage
-    excMaxAges = excMaxAges(excMaxAges > 2); % exclude ages ineligible for ART
+    excMaxAges = excMaxAges(excMaxAges > (ageSexDebut - 1)); % exclude ages ineligible for ART
     coverMax = (ageFracART - maxCoverLim) ./ ageFracART; % discontinuation fraction needed to meet max coverage
     formatMax = ones(1 , length(maxAges) , 1); 
     formatMax(:) = coverMax(maxAges); % set up matrix values by age
@@ -23,10 +23,11 @@ else
 end
 % Initiate persons in age groups with coverage < minCover
 minInds = ageFracART < minCoverLim; % find inds of ages below min coverage
-if sum(minInds) > 2
+if sum(minInds) > 0
     minAges = ageVec(minInds == 1); % ages below min coverage
-    minAges = minAges(minAges > 2); % exclude ages ineligible for ART
+    minAges = minAges(minAges > (ageSexDebut - 1)); % exclude ages ineligible for ART
     excMinAges = ageVec(~minInds); % ages that are good, ie above min coverage
+    excMinAges = excMinAges(excMinAges > (ageSexDebut - 1)); % exclude ages ineligible for ART
     coverMin = ((minCoverLim - ageFracART) ./ (1 - ageFracART)) .* ... % initiation needed to meet min coverage
         (ageHIVallSubTots ./ ageHIVeligSubTots); % adjust coverage b/c only initiating persons with eligible CD4 = ((coverMin*HIVtot)/HIVelig)
     formatMin = ones(1 , 1 , 1 , length(minAges) , 1);
