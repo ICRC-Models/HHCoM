38afb4220a1c6eabd51a43718827fec9264acb83
guiliu53 <guiliu@uw.edu>
2020-03-25 13:04:43 -0700

    changed partnersM multiplier

--- historicalSim.m
+++ historicalSim.m
@@ -34,7 +34,7 @@ tic
 
 % DIRECTORY TO SAVE RESULTS
 %pathModifier = ['toNow_' , date , '_noBaseVax_baseScreen_hpvHIVcalib_' , num2str(tstep_abc) , '_' , num2str(paramSetIdx)]; % ***SET ME***: name for historical run output file 
-pathModifier = 'toNow_9Mar20_Ken';
+pathModifier = 'toNow_25Mar20_Ken';
 
 % AGE GROUPS
 fivYrAgeGrpsOn = 1; % choose whether to use 5-year or 1-year age groups
