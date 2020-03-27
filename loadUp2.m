bfa1c1e383e9fc97151b5f078a5056e89947b470
guiliu53 <guiliu@uw.edu>
2020-03-27 10:30:11 -0700

    fixed sexual behaviors

--- loadUp2.m
+++ loadUp2.m
@@ -133,7 +133,7 @@ end
 fertility2 = fertility .* fertDeclineProp(1,1);
 fertility3 = fertility2 .* fertDeclineProp(2,1);
 
-partnersMmult = [2.5 5];
+partnersMmult = [2 4];
 % Male partners per year by age and risk group
 if calibBool && any(1 == pIdx)
     idx = find(1 == pIdx);
