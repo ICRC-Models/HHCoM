41bf5209e585933db15c9d5f45508e8a887d3048
guiliu53 <guiliu@uw.edu>
2020-03-26 19:02:41 -0700

    fixed sexual activity

--- mixInfect.m
+++ mixInfect.m
@@ -103,11 +103,11 @@ end
 
 %% Calculate mixing matrix rho (pattern of sexual contact by gender, age, risk)
 % partnership/ contact matrices
-if (year >= 1995) && (year < 2000)
+if (year >= 1995) && (year < 1998)
     dt = (year - 1995) * stepsPerYear;
     partnersMmult(1) = partnersMmult(1) + d_partnersMmult(1) .* dt;
     partnersMmult(2) = partnersMmult(2) + d_partnersMmult(2) .* dt;
-elseif year >= 2000
+elseif year >= 1998
     partnersMmult(1) = 1.2;
     partnersMmult(2) = 2.5;
 end
