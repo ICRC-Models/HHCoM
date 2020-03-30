9fe7bfbe267c9c0255d31f8d6c5d7ac0964ab76f
guiliu53 <guiliu@uw.edu>
2020-03-30 16:48:39 -0700

    changed partners decline

--- mixInfect.m
+++ mixInfect.m
@@ -116,13 +116,13 @@ end
 %% Calculate mixing matrix rho (pattern of sexual contact by gender, age, risk)
 % partnership/ contact matrices
 % partnersMmult = [2 4];
-if (year >= 1990) && (year < 1995)
+if (year >= 1993) && (year < 1998)
     dt = (year - 1990) * stepsPerYear;
     partnersMmult(1) = partnersMmult(1) + d_partnersMmult(1) .* dt;
     partnersMmult(2) = partnersMmult(2) + d_partnersMmult(2) .* dt;
-elseif year >= 1995
-    partnersMmult(1) = 1.2;
-    partnersMmult(2) = 2.5;
+elseif year >= 1998
+    partnersMmult(1) = 1.0;
+    partnersMmult(2) = 1.5;
 end
 partnersM(4:5, 1:3) = partnersM(4:5, 1:3) .* partnersMmult(1);
 partnersF(4:5, 1:3) = partnersF(4:5, 1:3) .* partnersMmult(2);
