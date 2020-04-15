d57e121925c50464ecca410f12977afcf35f05a1
guiliu53 <guiliu@uw.edu>
2020-04-14 17:16:03 -0700

    Partners multipliers for older people

--- mixInfect.m
+++ mixInfect.m
@@ -122,8 +122,8 @@ if (year >= 1995) && (year < 1999)
     partnersMmult(2) = partnersMmult(2) + d_partnersMmult(2) .* dt;
     partnersMmult(3) = partnersMmult(3) + d_partnersMmult(3) .* dt;
 elseif year >= 1999
-    partnersMmult(1) = 1.5;
-    partnersMmult(2) = 2.5;
+    partnersMmult(1) = 1.2;
+    partnersMmult(2) = 2.0;
     partnersMmult(3) = 0.9;
 end
 partnersM(4:5, 1:3) = partnersM(4:5, 1:3) .* partnersMmult(1);
