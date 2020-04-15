3443e8c4d6f2aa7bade31f3b478b7f9870fc80ed
guiliu53 <guiliu@uw.edu>
2020-04-14 17:15:45 -0700

    Partners multipliers for older people

--- historicalSim.m
+++ historicalSim.m
@@ -34,7 +34,7 @@ tic
 
 % DIRECTORY TO SAVE RESULTS
 %pathModifier = ['toNow_' , date , '_noBaseVax_baseScreen_hpvHIVcalib_' , num2str(tstep_abc) , '_' , num2str(paramSetIdx)]; % ***SET ME***: name for historical run output file 
-pathModifier = 'toNow_8Apr20_Ken_ApplyPartnersMultOlderAdults';
+pathModifier = 'toNow_13Apr20_Ken_ApplyPartnersMultOlderAdults_noCondom';
 
 % AGE GROUPS
 fivYrAgeGrpsOn = 1; % choose whether to use 5-year or 1-year age groups
