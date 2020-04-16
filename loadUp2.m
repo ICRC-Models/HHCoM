fffe773599fae70846a3c10d974cd0e62d8bba67
guiliu53 <guiliu@uw.edu>
2020-04-15 21:42:31 -0700

    Increase partners multiplier among women

--- loadUp2.m
+++ loadUp2.m
@@ -133,7 +133,7 @@ end
 fertility2 = fertility .* fertDeclineProp(1,1);
 fertility3 = fertility2 .* fertDeclineProp(2,1);
 
-partnersMmult = [1.9 4.5 1.5];
+partnersMmult = [1.9 5.5 1.5];
 % Male partners per year by age and risk group
 if calibBool && any(1 == pIdx)
     idx = find(1 == pIdx);
