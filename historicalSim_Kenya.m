a1d9693ed6840ee98eb43ba1ca3d1a0a2091e0a6
guiliu53 <guiliu@mox2.hyak.local>
2020-05-13 10:19:36 -0700

    add changes from Hyak

--- historicalSim_Kenya.m
+++ historicalSim_Kenya.m
@@ -34,7 +34,7 @@ tic
 
 % DIRECTORY TO SAVE RESULTS
 %pathModifier = ['toNow_' , date , '_noBaseVax_baseScreen_hpvHIVcalib_' , num2str(tstep_abc) , '_' , num2str(paramSetIdx)]; % ***SET ME***: name for historical run output file 
-pathModifier = 'toNow_12May20_KenNatClaib_riskDist005pct';
+pathModifier = 'toNow_12May20_KenNatClaib_riskAdj005pct_partnersMult';
 
 % AGE GROUPS
 fivYrAgeGrpsOn = 1; % choose whether to use 5-year or 1-year age groups
