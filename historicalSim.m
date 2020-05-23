460a4edd852e9acb43591793029a4d7e0e47c4c2
Gui Liu <guiliu@uw.edu>
2020-05-23 10:18:29 -0700

    decrease HPV transmission, increase HIV mulitplier on HPV inf

--- historicalSim.m
+++ historicalSim.m
@@ -35,7 +35,7 @@ tic
 % DIRECTORY TO SAVE RESULTS
 %pathModifier = ['toNow_' , date , '_noBaseVax_baseScreen_hpvHIVcalib_' , num2str(tstep_abc) , '_' , num2str(paramSetIdx)]; % ***SET ME***: name for historical run output file 
 
-pathModifier = 'N_ HPVtrans003_sexAct_HPVclear_mClear'; %Decrease HPV transmisson (0.0045), increase non-vax type transmission multuplier (1.45)
+pathModifier = 'toNow_23May20_N_HPVtrans0045_10pctHIVmult'; %Decrease HPV transmisson (0.0045), increase non-vax type transmission multuplier (1.45)
 
 % AGE GROUPS
 fivYrAgeGrpsOn = 1; % choose whether to use 5-year or 1-year age groups
