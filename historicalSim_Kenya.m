e12794a986a3a1a9d79415d4dbde6f17eeaee558
guiliu53 <guiliu@uw.edu>
2020-05-22 21:42:10 -0700

    Kenya: increase sexActs, decrease HPV transmission

--- historicalSim_Kenya.m
+++ historicalSim_Kenya.m
@@ -34,7 +34,7 @@ tic
 
 % DIRECTORY TO SAVE RESULTS
 %pathModifier = ['toNow_' , date , '_noBaseVax_baseScreen_hpvHIVcalib_' , num2str(tstep_abc) , '_' , num2str(paramSetIdx)]; % ***SET ME***: name for historical run output file 
-pathModifier = 'toNow_22May20_K_ HPVtrans01_2-1xsexAct_1-2xHPVclear_08mClear';
+pathModifier = 'toNow_22May20_K_ HPVtrans008_2-5xsexAct_1-5xHPVclear_08mClear';
 
 % AGE GROUPS
 fivYrAgeGrpsOn = 1; % choose whether to use 5-year or 1-year age groups
