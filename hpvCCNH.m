6dae7e953d2b10777c27ca2fb04c6e108de733f3
Gui Liu <guiliu@uw.edu>
2020-05-28 11:24:05 -0700

    reduce C3 to CC progression, increase HPV regression on ART

--- hpvCCNH.m
+++ hpvCCNH.m
@@ -55,7 +55,7 @@ for d = 1 : disease
         c2c1Mult = c2c1Mults(1); % CIN1 -> CIN2 progression multiplier
         c1c2Mult = hpv_hivClear(1); % CIN2 -> CIN1 regression multiplier
         rHivHpvMult = hpv_hivClear(1);%hpvClearMult(1); % Regression multiplier, compounds c1c2Mult
-        c2c3Mult = hpv_hivClear(1); % CIN3 -> CIN2 regression multiplier
+        c2c3Mult = hpv_hivClear(1) .* 1.1; % CIN3 -> CIN2 regression multiplier
         deathCC = muCC(5 , :); % HIV-positive on ART CC-associated mortality
     end
     
