2cea5e4a25aab45dd6d8116fe76fe4cfd85dea8d
Gui Liu <guiliu@uw.edu>
2020-05-28 16:20:52 -0700

    lower HIV mult on CIN2toCIN3 progression, lower mult on CIN3toCC

--- historicalSim.m
+++ historicalSim.m
@@ -35,7 +35,7 @@ tic
 % DIRECTORY TO SAVE RESULTS
 %pathModifier = ['toNow_' , date , '_noBaseVax_baseScreen_hpvHIVcalib_' , num2str(tstep_abc) , '_' , num2str(paramSetIdx)]; % ***SET ME***: name for historical run output file 
 
-pathModifier = 'toNow_28May20_N_01c3c2ART_2c3CC_09c3c2'; %Decrease HPV transmisson (0.0045), increase non-vax type transmission multuplier (1.45)
+pathModifier = 'toNow_28May20_N_1-5c2c3allHIV_1-8c3CC'; %Decrease HPV transmisson (0.0045), increase non-vax type transmission multuplier (1.45)
 
 % AGE GROUPS
 fivYrAgeGrpsOn = 1; % choose whether to use 5-year or 1-year age groups
--- historicalSim_Kenya.m
+++ historicalSim_Kenya.m
@@ -34,7 +34,7 @@ tic
 
 % DIRECTORY TO SAVE RESULTS
 %pathModifier = ['toNow_' , date , '_noBaseVax_baseScreen_hpvHIVcalib_' , num2str(tstep_abc) , '_' , num2str(paramSetIdx)]; % ***SET ME***: name for historical run output file 
-pathModifier = 'toNow_28May20_K_01c3c2ART_2c3CC_09c3c2';
+pathModifier = 'toNow_28May20_K_1-5c2c3allHIV_1-8c3CC';
 
 % AGE GROUPS
 fivYrAgeGrpsOn = 1; % choose whether to use 5-year or 1-year age groups
