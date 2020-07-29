b01cb0d6396d39bfffd7be6250162e624c05470d
Cara Bayer <carajb@uw.edu>
2020-07-29 13:35:45 -0700

    Decrease summed-log-likelihood cutoff to save fits

--- calib2_sumll4sets.m
+++ calib2_sumll4sets.m
@@ -68,7 +68,7 @@ dlmwrite([paramDir, file] , formatOutput , 'delimiter' , ',' , 'roffset' , 1 , '
 for j = 1 : nPrlSets
     pathModifier = ['toNow_' , date , '_noBaseVax_baseScreen_hpvHIVcalib_' , num2str(t_curr) , '_' , num2str(paramSetIdx + j - 1)];
     savdir = [pwd , '/HHCoM_Results/'];
-    if negSumLogLSet(j,1) < -700 %-200000.00
+    if negSumLogLSet(j,1) < -1500 %-200000.00
         delete([savdir , pathModifier , '.mat']);
     end
 end
