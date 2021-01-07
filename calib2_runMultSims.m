02e7a000c94b6805081663f57df6a8bfe5bab2cc
Cara Bayer <carajb@mox1.hyak.local>
2021-01-07 15:30:40 -0800

    Run 25 parallel future sims with reduced mem
    Now that the deaths output vector has been condensed to only save
    summed values by (disease, gender, age), run-time memory use is
    reduced. Therefore updating wrapper scripts to run 25 future sims
    in parallel.

--- calib2_runMultSims.m
+++ calib2_runMultSims.m
@@ -17,7 +17,7 @@ pc.JobStorageLocation = strcat('/gscratch/csde/carajb' , '/' , getenv('SLURM_JOB
 numCPUperNode = str2num(getenv('SLURM_CPUS_ON_NODE'))
 parpool(pc , numCPUperNode)    % start the pool with max number workers
 
-nPrlSets = 5; %numCPUperNode;
+nPrlSets = 25; %numCPUperNode;
 
 %% Load all particles
 paramDir = [pwd , '/Params/'];
@@ -48,13 +48,13 @@ for s = 1 : length(pIdx)
 end
 
 %% Obtain model output for each set of sampled parameters
-%for m = 1 : nPrlSets : numBestFits
-    subMatrixInds = [paramSetIdx : (paramSetIdx + nPrlSets - 1)];
-    %subMatrixInds = [m : (m + nPrlSets - 1)];
+for m = 1 : nPrlSets : numBestFits
+    %subMatrixInds = [paramSetIdx : (paramSetIdx + nPrlSets - 1)];
+    subMatrixInds = [m : (m + nPrlSets - 1)];
     parfor n = 1 : nPrlSets
         paramSet = top25Params(:,subMatrixInds(n));
         futureSim(1 , pIdx , paramsSub , paramSet , (paramSetIdx + n - 1) , tstep_abc , date_abc);
         %historicalSim(1 , pIdx , paramsSub , paramSet , (paramSetIdx + n - 1) , tstep_abc , date_abc);
     end
-%end
+end
 
