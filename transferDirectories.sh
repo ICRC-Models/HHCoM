fa39ba3061d46e889e085e73b68953d6aa99b7cd
Cara Bayer <carajb@uw.edu>
2021-01-08 14:51:26 -0800

    Update file paths for transfer

--- transferDirectories.sh
+++ transferDirectories.sh
@@ -1,9 +1,8 @@
 cd HHCoM_Results/
 
 for i in $(seq 1 1 25); do
-    #mkdir Vaccine22Apr20Ph2V2_baseVax057_baseScreen_baseVMMC_fertDec042-076-052_DoART_S1_11_$i
-	cd Vaccine22Apr20Ph2V2_baseVax057_baseScreen_baseVMMC_fertDec042-076-052_DoART_S1_11_$i
-	rm vaxSimResult2.mat
-	scp carajb@mox.hyak.uw.edu:/gscratch/csde/carajb/HHCoM/HHCoM_Results/Vaccine22Apr20Ph2V2_baseVax057_baseScreen_baseVMMC_fertDec042-076-052_DoART_S1_11_$i/*.mat .
+    mkdir Vaccine22Apr20Ph2V2_baseVax057_baseScreen_baseVMMC_fertDec042-076-052_2020ARTfxd_trackCD4-Discont_discontFxd_diagHiv075_DoART_S2_11_$i
+	cd Vaccine22Apr20Ph2V2_baseVax057_baseScreen_baseVMMC_fertDec042-076-052_2020ARTfxd_trackCD4-Discont_discontFxd_diagHiv075_DoART_S2_11_$i
+	scp carajb@mox.hyak.uw.edu:/gscratch/csde/carajb/HHCoM/HHCoM_Results/Vaccine22Apr20Ph2V2_baseVax057_baseScreen_baseVMMC_fertDec042-076-052_2020ARTfxd_trackCD4-Discont_discontFxd_diagHiv075_DoART_S2_11_$i/*.mat .
 	cd ..
 done
