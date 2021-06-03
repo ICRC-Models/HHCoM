61533ebf6c348e18ee5cdd7c0e55efef96bf0c8f
Cara Bayer <carajb@uw.edu>
2021-06-03 09:36:10 -0700

    Revise sces/plots/results in IPVC script

--- vaxCEA_multSims_IPVCsces.m
+++ vaxCEA_multSims_IPVCsces.m
@@ -55,21 +55,20 @@ paramDir = [pwd , '\Params\'];
 % ***SET ME***: save names of potential scenarios to analyze as variables
 baseDirName = 'Vaccine22Apr20Ph2V11_baseVax057_baseScreen_shortName_noVMMChpv_discontFxd_screenCovFxd_8ts-2021_';
 dirName_reductBaseline = [baseDirName , 'WHO-SCES0b_6_1'];
-dirName_reductBaseline2 = [baseDirName , 'WHO-SCES0b_gbV_6_1'];
-dirName_SCE2 = [baseDirName , 'WHO-SCES2_6_1'];
-dirName_SCE9 = [baseDirName , 'WHO-SCES6_6_1'];
-dirName_SCE9gbV = [baseDirName , 'WHO-SCES6_gbV_6_1'];
+dirName_SCE2_057 = [baseDirName , 'WHO-SCES2_057_6_1'];
+dirName_SCE2_057gbV = [baseDirName , 'WHO-SCES2_057gbV_6_1'];
+dirName_SCE7_057 = [baseDirName , 'WHO-SCES7_057_6_1']; % Note that these really should be named WHO-SCES8...
 
 % ***SET ME***: choose which scenarios you want to save data in Excel for
-simVec = {dirName_reductBaseline , dirName_reductBaseline2 , ...
-    dirName_SCE2 , dirName_SCE9 , dirName_SCE9gbV};
-fileVec = {'sim0' , 'sim0' , 'sim2' , 'sim2' , 'sim2'};
+simVec = {dirName_reductBaseline , dirName_SCE2_057 , ...
+    dirName_SCE2_057gbV , dirName_SCE7_057};
+fileVec = {'sim0' , 'sim2' , 'sim2' , 'sim2'};
+fileVec2 = {'0b' , '2_057' , '2_057gbV' ,'7_057'};
 % ***SET ME***: make sure the names here correspond to scenarios in simVec above
-fileTits = {'Baseline w/ 57% girls vax' , ...
-    '57% boys and girls vax' , ...
-    '90% girls multi-cohort vax' , ...
-    'WHO guidelines' , ...
-    'WHO guidelines + 90% boys multi-cohort vax'};
+fileTits = {'Baseline with 57% girls vax' , ...
+    '57% girls multi-cohort vax' , ...
+    '57% girls + boys multi-cohort vax' , ...
+    '57% girls multi-cohort + catch-up vax'};
 
 % Plot settings
 reset(0)
@@ -94,104 +93,146 @@ for j = 1 : nResults
         247167 240167 226750 201603 171975 150562 113118 82266 64484 42237 ...
         23477 9261 2155];
     
-    diseaseLabels = {'General' , 'HIV_neg' , 'HIV_posAll' , 'HIV_posNoArt' , 'HIV_posArt'};    
+    diseaseLabels = {'Pop(All) ICC' , 'HIV- (ICC)' , 'HIV+ (ICC)' , 'HIV+ no ART (ICC)' , 'HIV+ ART (ICC)'};    
     diseaseTits = {'General' , 'HIV-negative' , 'All WLHIV' , 'WLHIV-untreated' , 'WLHIV-on ART'};    
     
-    for dInd = 1 : 3 %length(diseaseLabels)
+    for dInd = 1 : 1 %length(diseaseLabels)
         % Load results
         fname = [pwd , '\HHCoM_Results\' , simVec{j} , '\' , ...
-            'ICC_' , diseaseLabels{dInd} , '_' , fileVec{j} , '.csv'];
-        ccIncHivAgeTime = xlsread(fname);
-        
-        ccIncRefTot = zeros(1 , size(ccIncHivAgeTime,2)-1);       
+            'UofW_Impact_CC_IncidenceRates-standardised-(2020-2120)_S' , fileVec2{j} , '.xlsx'];
+        ccIncHivAgeTime = readmatrix(fname , 'Sheet' , diseaseLabels{dInd} , 'Range' , ['C8:CZ21']);
+        ccIncHivAgeTime = [zeros(2 , size(ccIncHivAgeTime , 2)) ; ccIncHivAgeTime];
+
+        ccIncRefTot = zeros(1 , size(ccIncHivAgeTime,2));       
         for aInd = 1:age+4
             a = aInd;
             if aInd >= age
                 a = age;
             end
             if aInd <= age    
-                ccIncRef = ccIncHivAgeTime(a+1 , 2:end) .* worldStandard_WP2015(aInd);
+                ccIncRef = ccIncHivAgeTime(a , :) .* worldStandard_WP2015(aInd);
                 if (dInd == 5) && (a < 3)
-                    ccIncRef = zeros(1 , size(ccIncHivAgeTime,2)-1);
+                    ccIncRef = zeros(1 , size(ccIncHivAgeTime,2));
                 end
             elseif aInd > age
-                ccIncRef = ccIncHivAgeTime(a+1 , 2:end);
+                ccIncRef = ccIncHivAgeTime(a , :);
                 ccIncRef = [(ones(1,aInd-a).*ccIncRef(1,1)) , ccIncRef(1,1:end-(aInd-a))];
                 ccIncRef = ccIncRef .* worldStandard_WP2015(aInd);
             end
             ccIncRefTot = ccIncRefTot + ccIncRef;
         end
         ccInc = ccIncRefTot ./ (sum(worldStandard_WP2015(1:age+4)));
-        
-        ccIncRefTot_lb = zeros(1 , size(ccIncHivAgeTime,2)-1);       
-        for aInd = 1:age+4
-            a = aInd;
-            if aInd >= age
-                a = age;
-            end
-            if aInd <= age    
-                ccIncRef = ccIncHivAgeTime(a+1+age , 2:end) .* worldStandard_WP2015(aInd);
-                if (dInd == 5) && (a < 3)
-                    ccIncRef = zeros(1 , size(ccIncHivAgeTime,2)-1);
-                end
-            elseif aInd > age
-                ccIncRef = ccIncHivAgeTime(a+1+age , 2:end);
-                ccIncRef = [(ones(1,aInd-a).*ccIncRef(1,1)) , ccIncRef(1,1:end-(aInd-a))];
-                ccIncRef = ccIncRef .* worldStandard_WP2015(aInd);
-            end
-            ccIncRefTot_lb = ccIncRefTot_lb + ccIncRef;
-        end
-        ccInc_lb = ccIncRefTot_lb ./ (sum(worldStandard_WP2015(1:age+4)));
-        
-        ccIncRefTot_ub = zeros(1 , size(ccIncHivAgeTime,2)-1);       
-        for aInd = 1:age+4
-            a = aInd;
-            if aInd >= age
-                a = age;
-            end
-            if aInd <= age    
-                ccIncRef = ccIncHivAgeTime(a+1+age*2 , 2:end) .* worldStandard_WP2015(aInd);
-                if (dInd == 5) && (a < 3)
-                    ccIncRef = zeros(1 , size(ccIncHivAgeTime,2)-1);
-                end
-            elseif aInd > age
-                ccIncRef = ccIncHivAgeTime(a+1+age*2 , 2:end);
-                ccIncRef = [(ones(1,aInd-a).*ccIncRef(1,1)) , ccIncRef(1,1:end-(aInd-a))];
-                ccIncRef = ccIncRef .* worldStandard_WP2015(aInd);
-            end
-            ccIncRefTot_ub = ccIncRefTot_ub + ccIncRef;
-        end
-        ccInc_ub = ccIncRefTot_ub ./ (sum(worldStandard_WP2015(1:age+4)));
-          
+             
         % Plot baseline incidence
         if (j == 1) && (dInd == 1)
             fig = figure;
             set(fig,'DefaultAxesFontSize' , 18);
         end
-        subplot(1,3,dInd);
+        %subplot(1,3,dInd);
         hold all;
-        firstYrInd = (currYear-1-startYear)+2;
+        firstYrInd = (currYear-1-startYear)+1;
         firstYrInd2 = (currYear-1-startYear)+1;
-        p = plot(ccIncHivAgeTime(1 , firstYrInd:end) , ccInc(1 , firstYrInd2:end) , '-');
-%         hold all;
-%         x2 = [ccIncHivAgeTime(1 , firstYrInd:end) , fliplr(ccIncHivAgeTime(1 , firstYrInd:end))];
-%         inBetween = [ccInc_ub(1 , firstYrInd2:end) , fliplr(ccInc_lb(1 , firstYrInd2:end))];
-%         colorP = get(p,'Color');
-%         h = fill(x2 , inBetween , colorP);
-%         h.FaceAlpha = 0.3;
-%         h.LineStyle = '--';
-%         set(h,'EdgeColor', colorP);
-        axis([2020 2121 0 200])
+        p = plot([2020:2121] , ccInc(1 , :) , '-');
+        axis([2020 2121 0 80])
         grid on;
         xlabel('Year'); ylabel('AS ICC per 100K');
         title(diseaseTits{dInd});
         
         if j == nResults
             hold all;
-            plot (ccIncHivAgeTime(1 , 2:end) , ones(size(ccIncHivAgeTime,2)-1,1).*4.0 , 'k:')
+            plot ([2020:2121] , ones(1,size(ccIncHivAgeTime,2)).*4.0 , 'k:')
             hold all;
-            plot (ccIncHivAgeTime(1 , 2:end) , ones(size(ccIncHivAgeTime,2)-1,1).*10.0 , 'k--')
+            plot ([2020:2121] , ones(1,size(ccIncHivAgeTime,2)).*10.0 , 'k--')
         end  
     end
     legend(fileTits{:} , 'Elimination: <4/100K' , 'Benchmark: <10/100K');
+end
+
+for j = 1 : nResults 
+    %% Age-standardized HPV prevalence over time
+    % Note: the age-standardization process shifts the prevalence of the
+    % last modelled age group to the next age group in the following year.
+    % However, prevalence is NaN prior to HIV introduction in the
+    % HIV-positive no ART group, and NaN prior to ART introduction in the
+    % HIV-positive ART group. Since we have four age groups past the 16 we
+    % model, a NaN value is present for four years past the introduction of
+    % HIV/ART, leading to a NaN value for summed HPV infected during these 
+    % years. We therefore lack data in this four-year interval in the
+    % saved/plotted results.
+    worldStandard_WP2015 = [325428 311262 295693 287187 291738 299655 272348 ...
+        247167 240167 226750 201603 171975 150562 113118 82266 64484 42237 ...
+        23477 9261 2155];
+    
+    %diseaseLabels = {'Pop(All) (hrHPV)' , 'HIV-  (hrHPV)' , 'HIV+   (hrHPV)' , 'HIV+ no ART  (hrHPV)' , 'HIV+ ART  (hrHPV)'};
+    diseaseLabels = {'Pop(All) (vtHPV)' , 'HIV-  (vtHPV)' , 'HIV+   (vtHPV)' , 'HIV+ no ART  (vtHPV)' , 'HIV+ ART  (vtHPV)'};
+
+    for dInd = 1 : 1 %length(diseaseLabels)
+        % Load results
+        fname = [pwd , '\HHCoM_Results\' , simVec{j} , '\' , ...
+            'UofW_Impact_VThrHPV-MWcombined_Prevalence-standardised-(2020-2120)_S' , fileVec2{j} , '.xlsx'];
+        hpv_hivAgeW_dis = readmatrix(fname , 'Sheet' , diseaseLabels{dInd} , 'Range' , ['B4:CY4'])' ./ sum(worldStandard_WP2015);
+        
+        % Plot hrHPV prevalence
+        if (j == 1) && (dInd == 1)
+            fig = figure;
+            set(fig,'DefaultAxesFontSize' , 18);
+        end
+        %subplot(1,3,dInd);
+        hold all;
+        p = plot([2020 : 2121] , hpv_hivAgeW_dis , '-');
+        axis([2020 2121 0 0.07])
+        grid on;
+        xlabel('Year'); ylabel('9v-type hrHPV population prevalence');
+        title(diseaseTits{dInd});
+    end
+    legend(fileTits{:});
+end
+
+%% Calculate number vaccine doses per cervical cancer case averted
+fname = [pwd , '\HHCoM_Results\' , simVec{1} , '\' , ...
+    'CumulativeImpact_CC-standardised_wUncert-(2020-2120)_S' , fileVec2{1} , '.xlsx'];
+ccCumCasesTot_base = readmatrix(fname , 'Sheet' , 'Pop(All) CCC' , 'Range' , ['CW5:CW29']);
+
+for j = 1 : nResults 
+    fname = [pwd , '\HHCoM_Results\' , simVec{j} , '\' , ...
+        'CumulativeImpact_CC-standardised_wUncert-(2020-2120)_S' , fileVec2{j} , '.xlsx'];
+    ccCumCasesTot = readmatrix(fname , 'Sheet' , 'Pop(All) CCC' , 'Range' , ['CW5:CW29']);
+    ccCumCasesAverted = ccCumCasesTot_base - ccCumCasesTot;
+
+    fname = [pwd , '\HHCoM_Results\' , simVec{j} , '\' , ...
+        'RoutineSchoolVax_wUncert-(2021-2121)_S' , fileVec2{j} , '.xlsx'];
+    routineVaxTot = readmatrix(fname , 'Sheet' , 'Vaccinated (HIV+ on ART) (N)' , 'Range' , ['A4:A28']); % Note: sheet name doesn't make sense; accidently autopopulated variable from previous section
+    fname = [pwd , '\HHCoM_Results\' , simVec{j} , '\' , ...
+        'CUVax_wUncert-(2021-2121)_S' , fileVec2{j} , '.xlsx'];
+    cuVaxTot = readmatrix(fname , 'Sheet' , 'Vaccinated (HIV+ on ART) (N)' , 'Range' , ['A4:A28']); % Note: sheet name doesn't make sense; accidently autopopulated variable from previous section
+    vaxTot = routineVaxTot + cuVaxTot;
+    personsVaxTot = (routineVaxTot./2) + (cuVaxTot./3);
+    
+    vaxPerCase = vaxTot ./ ccCumCasesAverted;
+    vaxPersonsPerCase = personsVaxTot ./ ccCumCasesAverted;
+    
+    disp(['Scenario vaccine efficiency ' , num2str(j) , ': '])
+    disp(['Median: ' , num2str(median(vaxPerCase , 1))])
+    disp([' Minimum: ' , num2str(min(vaxPerCase , [] , 1))])
+    disp([' Maximum: ' , num2str(max(vaxPerCase , [] , 1))])
+    
+    disp(['Scenario vaccine efficiency per person ' , num2str(j) , ': '])
+    disp(['Median: ' , num2str(median(vaxPersonsPerCase , 1))])
+    disp([' Minimum: ' , num2str(min(vaxPersonsPerCase , [] , 1))])
+    disp([' Maximum: ' , num2str(max(vaxPersonsPerCase , [] , 1))])
+    
+    disp(['Scenario vaccine doses ' , num2str(j) , ': '])
+    disp(['Median: ' , num2str(median(vaxTot , 1))])
+    disp([' Minimum: ' , num2str(min(vaxTot , [] , 1))])
+    disp([' Maximum: ' , num2str(max(vaxTot , [] , 1))])
+    
+    disp(['Scenario # vaccinated ' , num2str(j) , ': '])
+    disp(['Median: ' , num2str(median(personsVaxTot , 1))])
+    disp([' Minimum: ' , num2str(min(personsVaxTot , [] , 1))])
+    disp([' Maximum: ' , num2str(max(personsVaxTot , [] , 1))])
+    
+    disp(['Scenario CC averted ' , num2str(j) , ': '])
+    disp(['Median: ' , num2str(median(ccCumCasesAverted , 1))])
+    disp([' Minimum: ' , num2str(min(ccCumCasesAverted , [] , 1))])
+    disp([' Maximum: ' , num2str(max(ccCumCasesAverted , [] , 1))])
 end
