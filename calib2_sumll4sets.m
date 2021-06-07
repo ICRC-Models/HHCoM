fedf9e01ef1494739b7de66e959defa114fde2d7
Cara Bayer <carajb@uw.edu>
2021-06-07 11:32:39 -0700

    Add username, path as user inputs to calib scripts

--- calib2_sumll4sets.m
+++ calib2_sumll4sets.m
@@ -6,7 +6,7 @@
 % Saves:
 % 1) File: negSumLogL_calib_[date].dat (negative log likelihood for each parameter set in sub-set)
 
-function calib2_sumll4sets(paramSetIdx , tstep_abc , date_abc)
+function calib2_sumll4sets(paramSetIdx , tstep_abc , date_abc , username)
 
 %delete(gcp('nocreate'));
 
@@ -17,7 +17,7 @@ date = date_abc;
 
 %% Cluster information
 pc = parcluster('local');    % create a local cluster object
-pc.JobStorageLocation = strcat('/gscratch/csde/carajb' , '/' , getenv('SLURM_JOB_ID'))    % explicitly set the JobStorageLocation to the temp directory that was created in the sbatch script
+pc.JobStorageLocation = strcat('/gscratch/csde/' , username , '/' , getenv('SLURM_JOB_ID'))    % explicitly set the JobStorageLocation to the temp directory that was created in the sbatch script
 numCPUperNode = str2num(getenv('SLURM_CPUS_ON_NODE'))
 parpool(pc , numCPUperNode)    % start the pool with max number workers
 
