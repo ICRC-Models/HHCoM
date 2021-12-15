f9c994daeefaf0deabd88023341b4c975d638cbf
Cara Bayer <carajb@mox2.hyak.local>
2021-12-15 06:03:21 -0800

    Update 22Nov21, t=1 calib files

--- loop_indices.sh
+++ loop_indices.sh
@@ -6,8 +6,8 @@ DIRPATH=/gscratch/csde/${USRNAME}/HHCoM    # SET ME: path to your HHCoM director
 echo "${DIRPATH}"
 export DIRPATH
 
-TMIN=0    # SET ME: calibration iteration to start with
-TMAX=0    # SET ME: calibration iteration to end on
+TMIN=1    # SET ME: calibration iteration to start with
+TMAX=1    # SET ME: calibration iteration to end on
 
 DATE=22Nov21    # SET ME: date identifier of calibration
 echo "${DATE}"
