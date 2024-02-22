a664c60e2b04ea94b64f68db49af8c677e25d849
clhchristine <christine.hathaway95@gmail.com>
2024-02-22 10:32:17 -0800

    Added beta distribution for KEN SHE single dose nonavalent results. Also did the bounding so 2 dose efficacy is always equal to or greater than 1 dose efficacy.

--- vaxEfficacyBetaProb_3Dose.R
+++ vaxEfficacyBetaProb_3Dose.R
@@ -13,8 +13,8 @@ library(openxlsx)
 # 2v 98.67 (74.4-100)
 
 # Set the median and confidence interval values
-median_val <- 0.967 # note this is actually the mean
-lower_ci <- 0.809
+median_val <- 0.988 # note this is actually the mean
+lower_ci <- 0.913
 upper_ci <- 0.998
 
 # Calculate the mean and standard deviation of the beta distribution
@@ -31,8 +31,8 @@ beta_dist_beta <- beta_dist_alpha*(1/beta_dist_mean - 1)
 result <- data.frame()
 
 
-for (alpha in seq(9, 9, 0.02)) {
-      for (beta in seq(0, 0.8, 0.01)) {
+for (alpha in seq(25, 27, 0.01)) {
+      for (beta in seq(0.3, 0.7, 0.005)) {
             lower_ci <- qbeta(0.025, alpha, beta)
             upper_ci <- qbeta(0.975, alpha, beta)
             median <- qbeta(0.5, alpha, beta)
@@ -46,17 +46,17 @@ for (alpha in seq(9, 9, 0.02)) {
 # filter and finding the optimal parameters 
 
 result %>% 
-      filter(lower >= 0.7, lower <= 0.8, 
+      filter(lower >= 0.90, #lower <= 0.8, 
              upper >= 0.98, upper <= 1, 
-             med >= 0.98 & med < 0.99) %>% 
+             med >= 0.98) %>%  # & med < 0.99) %>% 
       arrange(lower) %>% View()
 
 ## 2-valent study, Clinical Review Memo - Cervarix, October 15, 2009
 # 2v 98.67 (74.4-100)
 
 ## Setting the new alpha and beta 
-beta_dist_alpha <- 6.88
-beta_dist_beta <- 0.31
+beta_dist_alpha <- 26.94
+beta_dist_beta <- 0.615
 
 # Calculate the lower and upper 95% CI
 lower_ci <- qbeta(0.025, beta_dist_alpha, beta_dist_beta)
@@ -74,6 +74,69 @@ df <- data.frame(x = random_vals*100)
 # Create a data frame with the random values
 df <- data.frame(x = random_vals)
 
+# Setting the alpha and beta for the 2-dose
+beta_dist_alpha <- 26.94
+beta_dist_beta <- 0.615
+
+# Calculate the lower and upper 95% CI
+lower_ci <- qbeta(0.025, beta_dist_alpha, beta_dist_beta)
+upper_ci <- qbeta(0.975, beta_dist_alpha, beta_dist_beta)
+median <- qbeta(0.5, beta_dist_alpha, beta_dist_beta)
+
+# Generate random values from the beta distribution
+set.seed(123)
+random_vals <- rbeta(1000000, beta_dist_alpha, beta_dist_beta)
+
+
+# Create a data frame with the random values
+df <- data.frame(x = random_vals*100)
+
+# Create a data frame with the random values
+df <- data.frame(x = random_vals)
+
+## Setting the new alpha and beta 
+beta_dist_alpha <- 26.94
+beta_dist_beta <- 0.615
+
+# Calculate the lower and upper 95% CI
+lower_ci <- qbeta(0.025, beta_dist_alpha, beta_dist_beta)
+upper_ci <- qbeta(0.975, beta_dist_alpha, beta_dist_beta)
+median <- qbeta(0.5, beta_dist_alpha, beta_dist_beta)
+
+# Generate random values from the beta distribution
+set.seed(123)
+random_vals <- rbeta(1000000, beta_dist_alpha, beta_dist_beta)
+
+
+# Create a data frame with the random values
+df <- data.frame(x = random_vals*100)
+
+# Create a data frame with the random values
+df <- data.frame(x = random_vals)
+
+                        # Setting the alpha and beta for the 2-dose
+                        beta_dist_alpha <- 15.66
+                        beta_dist_beta <- 0.82
+                        
+                        # Calculate the lower and upper 95% CI
+                        lower_ci <- qbeta(0.025, beta_dist_alpha, beta_dist_beta)
+                        upper_ci <- qbeta(0.975, beta_dist_alpha, beta_dist_beta)
+                        median <- qbeta(0.5, beta_dist_alpha, beta_dist_beta)
+                        
+                        # Generate random values from the beta distribution
+                        set.seed(123)
+                        random_vals <- rbeta(1000000, beta_dist_alpha, beta_dist_beta)
+                        
+                        # Create a data frame with the random values
+                        df_9v_2d <- data.frame(x = random_vals)
+
+bounded <- df %>% 
+      bind_cols(df_9v_2d) %>% 
+      filter(`x...2` >= `x...1`) 
+
+dose1_9v <- bounded %>% select(`x...1`) %>% rename(x=`x...1`)
+dose2_9v <- bounded %>% select(`x...2`) %>% rename(x=`x...2`)
+
 # Generate the plot using ggplot2
 ggplot(df, aes(x = x)) +
       stat_density(aes(y = ..scaled..), fill = "#69b3a2", alpha = 0.6) +
@@ -89,6 +152,15 @@ OUT <- createWorkbook()
 
 addWorksheet(OUT, "VaxEfficacyRandVal")
 
-writeData(OUT, x = random_vals, startRow = 1, sheet="VaxEfficacyRandVal")
+writeData(OUT, x = dose2_9v, startRow = 1, sheet="VaxEfficacyRandVal")
 
-saveWorkbook(OUT, "/Users/clh89/MATLAB/Projects/ccTreatment_KZN/Params/VaxEfficacyRandVal_2dose_bivalent.xlsx", overwrite = TRUE)
+saveWorkbook(OUT, "/Users/clh89/MATLAB/Projects/ccTreatment_KZN/Params/VaxEfficacyRandVal_2dose_nonavalent_bounded.xlsx", overwrite = TRUE)
+
+
+OUT <- createWorkbook()
+
+addWorksheet(OUT, "VaxEfficacyRandVal")
+
+writeData(OUT, x = dose1_9v, startRow = 1, sheet="VaxEfficacyRandVal")
+
+saveWorkbook(OUT, "/Users/clh89/MATLAB/Projects/ccTreatment_KZN/Params/VaxEfficacyRandVal_1dose_nonavalent_bounded.xlsx", overwrite = TRUE)
